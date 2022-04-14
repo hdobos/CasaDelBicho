fba336a64f5f54dbc0edc68fea28134fef0461b0
Hannah (Orion) Dobos <56137148+gobos12@users.noreply.github.com>
2022-04-13 18:11:24 -0700

    making recipes for the crafting menu

--- Casa Del Bicho/Assets/Scripts/CraftScripts/CraftingManager.cs
+++ Casa Del Bicho/Assets/Scripts/CraftScripts/CraftingManager.cs
@@ -9,11 +9,12 @@ public class CraftingManager : MonoBehaviour
     public Image customCursor;
     public Slot[] craftingSlots;
     public Item[] items;
+    public Sprite[] Results;
 
 
     private void Update(){
         if(Input.GetMouseButtonUp(0)){
-            if(currentItem != null){
+            if(currentItem != null && currentItem.getAmount() > 0){
                 customCursor.gameObject.SetActive(false);
                 Slot nearestSlot = null;
                 float shortestDistance = float.MaxValue;
@@ -28,6 +29,8 @@ public class CraftingManager : MonoBehaviour
                 }
                 nearestSlot.gameObject.SetActive(true);
                 nearestSlot.GetComponent<Image>().sprite = currentItem.GetComponent<Image>().sprite;
+                nearestSlot.item = currentItem;
+                currentItem.Amount--;
                 currentItem = null;
             }
         }
@@ -41,8 +44,24 @@ public class CraftingManager : MonoBehaviour
     public void OnMouseDownItem(Item item){
         if(currentItem == null){
             currentItem = item;
-            customCursor.gameObject.SetActive(true);
-            customCursor.sprite = currentItem.GetComponent<Image>().sprite;
+            if(currentItem.getAmount() > 0){
+                customCursor.gameObject.SetActive(true);
+                customCursor.sprite = currentItem.GetComponent<Image>().sprite;
+            }
+        }
+    }
+
+    public void Craft(){
+        if(craftingSlots[0].getItem() == items[0] && craftingSlots[1].getItem() == items[1] && craftingSlots[2].getItem() == items[2]){
+            Debug.Log("craft test");
+            craftingSlots[3].GetComponent<Image>().sprite = Results[0];
+        }
+        else{
+            foreach(Slot slot in craftingSlots){
+                if(slot.getItem() != null){
+                    slot.getItem().Amount++;
+                }
+            }
         }
     }
 
--- Casa Del Bicho/Assets/Scripts/CraftScripts/CraftingRecipe.cs
+++ Casa Del Bicho/Assets/Scripts/CraftScripts/CraftingRecipe.cs
@@ -3,23 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable]
-public struct ItemAmount
+// [CreateAssetMenu]
+public class CraftingRecipe : MonoBehaviour
 {
-    public Item Item;
-    [Range(1, 10)]
-    public int Amount;
-}
-
-[CreateAssetMenu]
-public class CraftingRecipe : ScriptableObject
-{
-    public List<ItemAmount> Materials;
-    public List<ItemAmount> Results;
+    public Item[] Materials;
+    public Item[] Results;
 
     public bool CanCraft(IItemContainer itemContainer){
-        foreach(ItemAmount itemAmount in Materials){
-            if(itemContainer.ItemCount(itemAmount.Item) < itemAmount.Amount){
+        foreach(Item item in Materials){
+            if(itemContainer.ItemCount(item) < item.Amount){
                 return false;
             }
         }
@@ -28,17 +20,18 @@ public class CraftingRecipe : ScriptableObject
 
     public void Craft(IItemContainer itemContainer){
         if(CanCraft(itemContainer)){
-            foreach(ItemAmount itemAmount in Materials){
-                for(int i = 0; i < itemAmount.Amount; i++){
-                    itemContainer.RemoveItem(itemAmount.Item);
+            foreach(Item item in Materials){
+                for(int i = 0; i < item.Amount; i++){
+                    itemContainer.RemoveItem(item);
                 }
             }
 
-            foreach(ItemAmount itemAmount in Results){
-                for(int i = 0; i < itemAmount.Amount; i++){
-                    itemContainer.AddItem(itemAmount.Item);
+            foreach(Item item in Results){
+                for(int i = 0; i < item.Amount; i++){
+                    itemContainer.AddItem(item);
                 }
             }
         }
     }
+
 }

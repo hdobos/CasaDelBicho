85d1683c1bc3025daac64e427c7d61edbca5abc7
Hannah (Orion) Dobos <56137148+gobos12@users.noreply.github.com>
2022-04-22 23:52:14 -0700

    updating craftin menu
    ASHDJSAHFJAGAJ IR:JIAJOUIGNLSJFNOIS

---
+++ Casa Del Bicho/Assets/Scripts/newCraftingScripts/SlotTemplate.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SlotTemplate : MonoBehaviour, IPointerClickHandler
+{
+    public Image container;
+    public Image item;
+    public Text count;
+
+    [HideInInspector]
+    public bool hasClicked = false;
+    
+    [HideInInspector]
+    public CraftItem craftController;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        hasClicked = true;
+        //craftController.ClickEventRecheck();
+    }
+}

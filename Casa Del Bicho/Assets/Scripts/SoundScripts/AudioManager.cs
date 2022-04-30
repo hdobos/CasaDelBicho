fc87bd2cd03e0cde091fda8aaff29fb8eea587e1
Hannah (Orion) Dobos <56137148+gobos12@users.noreply.github.com>
2022-04-30 02:08:07 -0700

    adding sounds among other things

---
+++ Casa Del Bicho/Assets/Scripts/SoundScripts/AudioManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        if(instance == null) instance = this;
+        else{
+            Destroy(gameObject);
+            return;
+        }
+
+        foreach(Sound s in sounds){
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.mute = s.mute;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sounds => sounds.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound:" + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sounds => sounds.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound:" + name + " not found!");
+            return;
+        }
+        s.source.Stop();
+    }
+    
+}
